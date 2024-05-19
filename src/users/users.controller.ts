015d7c37a79179f45eb634ad244063896d74d385
Anton Bochkovskyi <unundertale1@ukr.net>
2024-05-19 14:31:06 +0300

    use users service for create user functionality

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common'
 import { CreateUserDTO } from './dto/create-user.dto'
 import { ApiTags } from '@nestjs/swagger'
+import { UsersService } from './users.service'
 
 @ApiTags('auth')
 @Controller('auth')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
+
   @Post('signup')
   createUser(@Body() body: CreateUserDTO) {
-    console.log(body)
+    return this.usersService.create(body)
   }
 }
