478bdd8b700f6972bb3c16e4c71363320f0f0c96
Anton Bochkovskyi <unundertale1@ukr.net>
2024-05-19 14:30:19 +0300

    add create function

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common'
+import { Prisma, User } from '@prisma/client'
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const { email, password } = data
+
+    return this.prisma.user.create({
+      data: { email, password },
+    })
+  }
+}
