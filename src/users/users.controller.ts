5aa89a3d373b20221a383cbf556ccbbfc1c02286
Anton Bochkovskyi <unundertale1@ukr.net>
2024-05-19 16:54:32 +0300

    add update user function

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common'
 import { CreateUserDTO } from './dto/create-user.dto'
 import { ApiQuery, ApiTags } from '@nestjs/swagger'
 import { UsersService } from './users.service'
+import { UpdateUserDTO } from './dto/update-user.dto'
 
 @ApiTags('auth')
 @Controller('auth')
@@ -16,11 +25,18 @@ export class UsersController {
 
   @Get(':id')
   getUserById(@Param('id') id: string) {
-    return this.usersService.findOneById(parseInt(id, 10))
+    const parsedId = parseInt(id, 10)
+    return this.usersService.findOneById(parsedId)
   }
 
   @Post('signup')
   createUser(@Body() body: CreateUserDTO) {
     return this.usersService.create(body)
+  }
+
+  @Patch(':id')
+  updateUser(@Param('id') id: string, @Body() body: UpdateUserDTO) {
+    const parsedId = parseInt(id, 10)
+    return this.usersService.update(parsedId, body)
   }
 }
