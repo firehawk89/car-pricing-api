e5d51f7a4fcc115200940e58cd600cac2d7b2042
Anton Bochkovskyi <unundertale1@ukr.net>
2024-05-19 16:32:19 +0300

    add get user functions

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common'
 import { CreateUserDTO } from './dto/create-user.dto'
-import { ApiTags } from '@nestjs/swagger'
+import { ApiQuery, ApiTags } from '@nestjs/swagger'
 import { UsersService } from './users.service'
 
 @ApiTags('auth')
 @Controller('auth')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @ApiQuery({ name: 'email', required: false })
+  @Get()
+  getUserByEmail(@Query('email') email: string) {
+    return this.usersService.findOneByEmail(email)
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return this.usersService.findOneById(parseInt(id, 10))
+  }
 
   @Post('signup')
   createUser(@Body() body: CreateUserDTO) {
