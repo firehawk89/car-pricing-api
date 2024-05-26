397aca6b690467e540a05ff102f7b91b8fe025fa
Anton Bochkovskyi <unundertale1@ukr.net>
2024-05-26 20:01:00 +0300

    define current user interceptor as global

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,16 @@ import { UsersController } from './users.controller'
 import { UsersService } from './users.service'
 import { PrismaModule } from 'src/prisma/prisma.module'
 import { AuthService } from './auth.service'
+import { CurrentUserInterceptor } from './interceptors/current-user.interceptor'
+import { APP_INTERCEPTOR } from '@nestjs/core'
 
 @Module({
   imports: [PrismaModule],
   controllers: [UsersController],
-  providers: [UsersService, AuthService],
+  providers: [
+    UsersService,
+    AuthService,
+    { useClass: CurrentUserInterceptor, provide: APP_INTERCEPTOR },
+  ],
 })
 export class UsersModule {}
