8e99a636d9121ffccf66b38f33233cefc0420e0a
Anton Bochkovskyi <unundertale1@ukr.net>
2024-06-08 22:03:10 +0300

    add register with used email test

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,12 +2,14 @@ import { Test } from '@nestjs/testing'
 import { AuthService } from './auth.service'
 import { UsersService } from './users.service'
 import { Prisma } from '@prisma/client'
+import { BadRequestException } from '@nestjs/common'
 
 describe('AuthService', () => {
   let authService: AuthService
+  let fakeUsersService: Partial<UsersService>
 
   beforeEach(async () => {
-    const fakeUsersService: Partial<UsersService> = {
+    fakeUsersService = {
       findOneByEmail: () => Promise.resolve(null),
       create: ({ email, password }: Prisma.UserCreateInput) =>
         Promise.resolve({
@@ -29,6 +31,25 @@ describe('AuthService', () => {
 
   it('can create an instance of auth service', async () => {
     expect(authService).toBeDefined()
+  })
+
+  it('throws an error if user tries to register with an email that is in use', async () => {
+    const email = 'testing_user@email.com'
+    const password = 'testing_user'
+
+    fakeUsersService.findOneByEmail = () =>
+      Promise.resolve({
+        user_id: 1,
+        email,
+        password,
+      })
+
+    await expect(
+      authService.register({
+        email,
+        password,
+      }),
+    ).rejects.toThrow(BadRequestException)
   })
 
   it('creates a new user with a salted and hashed password', async () => {
