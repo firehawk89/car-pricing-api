bafc4c9d9916315e00606fccb8470d0ef4793ce2
Anton Bochkovskyi <unundertale1@ukr.net>
2024-06-08 20:56:44 +0300

    add test describe and beforeEach

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,25 +3,31 @@ import { AuthService } from './auth.service'
 import { UsersService } from './users.service'
 import { Prisma } from '@prisma/client'
 
-it('can create an instance of auth service', async () => {
-  const fakeUsersService: Partial<UsersService> = {
-    findOneByEmail: () => Promise.resolve(null),
-    create: ({ email, password }: Prisma.UserCreateInput) =>
-      Promise.resolve({
-        user_id: 1,
-        email,
-        password,
-      }),
-  }
+describe('AuthService', () => {
+  let authService: AuthService
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      { provide: UsersService, useValue: fakeUsersService },
-    ],
-  }).compile()
+  beforeEach(async () => {
+    const fakeUsersService: Partial<UsersService> = {
+      findOneByEmail: () => Promise.resolve(null),
+      create: ({ email, password }: Prisma.UserCreateInput) =>
+        Promise.resolve({
+          user_id: 1,
+          email,
+          password,
+        }),
+    }
 
-  const authService = module.get(AuthService)
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: fakeUsersService },
+      ],
+    }).compile()
 
-  expect(authService).toBeDefined()
+    authService = module.get<AuthService>(AuthService)
+  })
+
+  it('can create an instance of auth service', async () => {
+    expect(authService).toBeDefined()
+  })
 })
