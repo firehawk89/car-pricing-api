2fd0c642647623784598a0fd2f72ae37a2e78f90
Anton Bochkovskyi <unundertale1@ukr.net>
2024-06-09 19:43:56 +0300

    add users controller tests, implement fake users service

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -4,12 +4,12 @@ import { UsersService } from './users.service'
 import { Prisma, User } from '@prisma/client'
 import { BadRequestException, NotFoundException } from '@nestjs/common'
 
+const FAKE_EMAIL = 'testing_user@email.com'
+const FAKE_PASSWORD = 'testing_user'
+
 describe('AuthService', () => {
   let authService: AuthService
   let fakeUsersService: Partial<UsersService>
-
-  const fakeEmail = 'testing_user@email.com'
-  const fakePassword = 'testing_user'
 
   beforeEach(async () => {
     const users: User[] = []
@@ -43,27 +43,27 @@ describe('AuthService', () => {
 
   it('throws an error if user tries to register with an email that is in use', async () => {
     await authService.register({
-      email: fakeEmail,
-      password: fakePassword,
+      email: FAKE_EMAIL,
+      password: FAKE_PASSWORD,
     })
 
     await expect(
       authService.register({
-        email: fakeEmail,
-        password: fakePassword,
+        email: FAKE_EMAIL,
+        password: FAKE_PASSWORD,
       }),
     ).rejects.toThrow(BadRequestException)
   })
 
   it('creates a new user with a salted and hashed password', async () => {
     const user = await authService.register({
-      email: fakeEmail,
-      password: fakePassword,
+      email: FAKE_EMAIL,
+      password: FAKE_PASSWORD,
     })
 
     const [salt, hash] = user.password.split('.')
 
-    expect(user.password).not.toEqual(fakePassword)
+    expect(user.password).not.toEqual(FAKE_PASSWORD)
     expect(salt).toBeDefined()
     expect(hash).toBeDefined()
   })
@@ -71,8 +71,8 @@ describe('AuthService', () => {
   it('throws an error if user tries to sign in with an email which not exists', async () => {
     await expect(
       authService.authenticate({
-        email: fakeEmail,
-        password: fakePassword,
+        email: FAKE_EMAIL,
+        password: FAKE_PASSWORD,
       }),
     ).rejects.toThrow(NotFoundException)
   })
@@ -81,24 +81,24 @@ describe('AuthService', () => {
     const wrongPassword = 'wrong_password'
 
     await authService.register({
-      email: fakeEmail,
-      password: fakePassword,
+      email: FAKE_EMAIL,
+      password: FAKE_PASSWORD,
     })
 
     await expect(
       authService.authenticate({
-        email: fakeEmail,
+        email: FAKE_EMAIL,
         password: wrongPassword,
       }),
     ).rejects.toThrow(BadRequestException)
   })
 
   it('returns a user if password is correct', async () => {
-    await authService.register({ email: fakeEmail, password: fakePassword })
+    await authService.register({ email: FAKE_EMAIL, password: FAKE_PASSWORD })
 
     const user = await authService.authenticate({
-      email: fakeEmail,
-      password: fakePassword,
+      email: FAKE_EMAIL,
+      password: FAKE_PASSWORD,
     })
 
     expect(user).toBeDefined()
---
+++ src/users/users.controller.spec.ts
@@ -0,0 +1,62 @@
+import { Test, TestingModule } from '@nestjs/testing'
+import { UsersController } from './users.controller'
+import { UsersService } from './users.service'
+import { Prisma, User } from '@prisma/client'
+
+const FAKE_EMAIL = 'testing_user@email.com'
+const FAKE_PASSWORD = 'testing_user'
+
+describe('UsersController', () => {
+  let usersController: UsersController
+  let fakeUsersService: Partial<UsersService>
+
+  beforeEach(async () => {
+    let users: User[] = [
+      { user_id: 1, email: `1_${FAKE_EMAIL}`, password: FAKE_PASSWORD },
+      { user_id: 2, email: `2_${FAKE_EMAIL}`, password: FAKE_PASSWORD },
+      { user_id: 3, email: `3_${FAKE_EMAIL}`, password: FAKE_PASSWORD },
+    ]
+
+    fakeUsersService = {
+      findAll: () => {
+        return Promise.resolve(users)
+      },
+      findOneById: (id: string) => {
+        const parsedId = parseInt(id, 10)
+        const user = users.find((user) => user.user_id === parsedId)
+        return Promise.resolve(user)
+      },
+      findOneByEmail: (email: string) => {
+        const user = users.find((user) => user.email === email)
+        return Promise.resolve(user)
+      },
+      update: (id: string, data: Partial<Prisma.UserUpdateInput>) => {
+        const parsedId = parseInt(id, 10)
+        let user = users.find((user) => user.user_id === parsedId)
+        user = {
+          ...user,
+          email: data.email as string,
+          password: data.password as string,
+        }
+        return Promise.resolve(user)
+      },
+      delete: (id: string) => {
+        const parsedId = parseInt(id, 10)
+        const user = users.find((user) => user.user_id === parsedId)
+        users = users.filter((user) => user.user_id !== parsedId)
+        return Promise.resolve(user)
+      },
+    }
+
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [UsersController],
+      providers: [{ provide: UsersService, useValue: fakeUsersService }],
+    }).compile()
+
+    usersController = module.get<UsersController>(UsersController)
+  })
+
+  it('should be defined', () => {
+    expect(usersController).toBeDefined()
+  })
+})
