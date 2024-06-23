af4b81ac4a1550169b7a5a4a1530ec7a7f43ba78
Anton Bochkovskyi <unundertale1@ukr.net>
2024-06-23 17:43:50 +0300

    add constants file

---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+export const MIN_YEAR = 1930
+export const MAX_YEAR = new Date().getFullYear()
+
+export const SALT_LENGTH = 16
+export const KEY_LENGTH = 32
+
+export const FAKE_EMAIL = 'user_test@email.com'
+export const FAKE_PASSWORD = 'user_test'
