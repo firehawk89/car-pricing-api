9b5da71b6c368bf55ad7ffd0d5ebe85a32b2fc99
Anton Bochkovskyi <unundertale1@ukr.net>
2024-07-06 20:21:53 +0300

    add validation constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,9 @@
+export const MIN_PRICE = 0
+export const MAX_PRICE = 1_000_000
 export const MIN_YEAR = 1930
 export const MAX_YEAR = new Date().getFullYear()
+export const MIN_MILEAGE = 0
+export const MAX_MILEAGE = 1_000_000
 
 export const SALT_LENGTH = 16
 export const KEY_LENGTH = 32
