13ea39636dfe9d787409126e4c8d940a2fcc4894
Anton Bochkovskyi <unundertale1@ukr.net>
2024-07-06 20:24:14 +0300

    add reports service with create method

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/prisma/prisma.service'
+import { CreateReportDTO } from './dto/create-report.dto'
+import { Report } from '@prisma/client'
+import { UserDTO } from 'src/users/dto/user.dto'
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: CreateReportDTO, user: UserDTO): Promise<Report> {
+    return this.prisma.report.create({
+      data: { ...data, user_id: user.user_id },
+    })
+  }
+}
