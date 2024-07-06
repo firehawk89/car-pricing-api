7f69c2e41eb86bdc901ba0a708123ec035cc32f6
Anton Bochkovskyi <unundertale1@ukr.net>
2024-07-06 20:48:25 +0300

    add approveReport controller

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,22 +1,42 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  NotFoundException,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { CreateReportDTO } from './dto/create-report.dto'
 import { ReportsService } from './reports.service'
 import { AuthGuard } from 'src/guards/auth.guard'
 import { CurrentUser } from 'src/users/decorators/current-user.decorator'
 import { UserDTO } from 'src/users/dto/user.dto'
+import { ApproveReportDTO } from './dto/approve-report.dto'
 
 @ApiTags('reports')
+@UseGuards(AuthGuard)
 @Controller('reports')
 export class ReportsController {
   constructor(private reportsService: ReportsService) {}
 
-  @UseGuards(AuthGuard)
   @Post()
   async createReport(
     @Body() body: CreateReportDTO,
     @CurrentUser() user: UserDTO,
   ) {
     return this.reportsService.create(body, user)
+  }
+
+  @Patch(':id')
+  async approveReport(@Param('id') id: string, @Body() body: ApproveReportDTO) {
+    const report = await this.reportsService.findOneById(id)
+    if (!report) {
+      throw new NotFoundException(`Report with ID ${id} not found`)
+    }
+
+    const { approved } = body
+    return this.reportsService.updateApproval(id, approved)
   }
 }
