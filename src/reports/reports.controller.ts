e4b339e8783452d6ba41fb3cf16d50c08b569f20
Anton Bochkovskyi <unundertale1@ukr.net>
2024-07-06 20:24:34 +0300

    add reports controller with createReport method

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Post, UseGuards } from '@nestjs/common'
+import { ApiTags } from '@nestjs/swagger'
+import { CreateReportDTO } from './dto/create-report.dto'
+import { ReportsService } from './reports.service'
+import { AuthGuard } from 'src/guards/auth.guard'
+import { CurrentUser } from 'src/users/decorators/current-user.decorator'
+import { UserDTO } from 'src/users/dto/user.dto'
 
+@ApiTags('reports')
 @Controller('reports')
-export class ReportsController {}
+export class ReportsController {
+  constructor(private reportsService: ReportsService) {}
+
+  @UseGuards(AuthGuard)
+  @Post()
+  async createReport(
+    @Body() body: CreateReportDTO,
+    @CurrentUser() user: UserDTO,
+  ) {
+    return this.reportsService.create(body, user)
+  }
+}
