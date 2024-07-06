ec8ba7e1d7d67bc15b692bf3e908933bff27c334
Anton Bochkovskyi <unundertale1@ukr.net>
2024-07-06 20:48:01 +0300

    add findOneById and updateApproval services

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -8,9 +8,24 @@ import { UserDTO } from 'src/users/dto/user.dto'
 export class ReportsService {
   constructor(private prisma: PrismaService) {}
 
+  async findOneById(id: string): Promise<Report> {
+    const parsedId = parseInt(id, 10)
+    return this.prisma.report.findUnique({
+      where: { report_id: parsedId },
+    })
+  }
+
   async create(data: CreateReportDTO, user: UserDTO): Promise<Report> {
     return this.prisma.report.create({
       data: { ...data, user_id: user.user_id },
+    })
+  }
+
+  async updateApproval(id: string, approved: boolean): Promise<Report> {
+    const parsedId = parseInt(id, 10)
+    return this.prisma.report.update({
+      where: { report_id: parsedId },
+      data: { approved },
     })
   }
 }
